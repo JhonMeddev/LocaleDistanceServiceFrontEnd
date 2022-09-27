d0849a58d340d89e2967094830e1bba7d54176d9
Jhonatan Medeiros <jhonatan1medeiros@gmail.com>
2022-09-27 12:16:23 -0300

    Consumindo api

---
+++ script.js
@@ -0,0 +1,9 @@
+function testeAJAX(){
+    var a = $("#a").val();
+    var b = $("#b").val();
+    
+    $.post( "soma.php", { num1: a, num2: b })
+        .done(function( data ) {
+            $("#r").html(data);
+        });
+}
