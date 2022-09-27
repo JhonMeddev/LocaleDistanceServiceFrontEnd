387a7615a0b87391886be2e1d37a1d6433226938
Jhonatan Medeiros <jhonatan1medeiros@gmail.com>
2022-09-27 16:55:56 -0300

    0.0.1

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
-function testeAJAX(){
-    var a = $("#a").val();
-    var b = $("#b").val();
-    
-    $.post( "soma.php", { num1: a, num2: b })
-        .done(function( data ) {
-            $("#r").html(data);
+
+        $(document).ready(function() {
+
+            function limpa_formulário() {
+                // Limpa valores do formulário de coordenadas.
+                $("#n1").text("");
+                $("#n2").text("");
+            }
+            
+            //Quando o campo cep perde o foco.
+            $("#n2").blur(function() {
+
+                //Novas variáveis "n1" e "n2".
+                var n1 = $("#n1").val();
+                var n2 = $("#n2").val();
+
+                //Verifica se os campos possui valor informado.
+                if (n1 != "" && n2 != "") {
+
+                    $("#placeholder").text("")
+                    $(".distancia").css("display","flex")
+
+                        //Preenche os campos com "..." enquanto consulta webservice.
+                        $("#distancia").val("...");
+
+                        //Consulta o webservice viacep.com.br/
+                        $.getJSON("https://router.project-osrm.org/route/v1/driving/"+ n1 + ";" + n2 , function(dados) {
+
+                            if (!("erro" in dados)) {
+                                //Atualiza os campos com os valores da consulta.
+                                $("#local1").text(dados.waypoints[0].name);
+                                $("#local2").text(dados.waypoints[1].name);
+                                $("#distancia").text(dados.routes[0].distance + " KM");
+
+                            } //end if.
+                            else {
+                                //CEP pesquisado não foi encontrado.
+                                limpa_formulário();
+                                alert("Coordenadas não encontrado.");
+                            }
+                        });
+
+                } //end if.
+                else {
+                    //cep sem valor, limpa formulário.
+                    limpa_formulário();
+                }
+            });
+
         });
-}
